55045ec70fdab2aa68b4e904dfb9f096f70da8ce
Nora Naceri <nora.naceri90@gmail.com>
2020-11-26 20:38:17 +0100

    ajout de la catégorie dans le formulaire

--- blog/src/Form/ArticleType.php
+++ blog/src/Form/ArticleType.php
@@ -3,6 +3,8 @@
 namespace App\Form;
 
 use App\Entity\Article;
+use App\Entity\Category;
+use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
@@ -20,6 +22,10 @@ class ArticleType extends AbstractType
                     "placeholder" => "Titre de l'article"
                     ]
                 ])
+            ->add('category', EntityType::class, [
+                'class' => Category::class,
+                'choice_label' => 'title'
+            ])
             ->add('content', TextareaType::class, [
                 'attr' => [
                     "placeholder" => "Contenu de l'article"
