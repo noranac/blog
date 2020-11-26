7d5cf9520c4af26397930d7fdc4f495f627a3b97
Nora Naceri <nora.naceri90@gmail.com>
2020-11-26 15:59:26 +0100

    mise en page du blog + creation du BlogController

---
+++ blog/src/Controller/BlogController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    /**
+     * @Route("/blog", name="blog")
+     */
+    public function index(): Response
+    {
+        return $this->render('blog/index.html.twig', [
+            'controller_name' => 'BlogController',
+        ]);
+    }
+
+    /**
+     * @Route("/", name="home")
+     */
+    public function home(): Response
+    {
+        return $this->render('blog/home.html.twig', [
+            'title' => 'Bienvenue sur mon blog',
+        ]);
+    }
+
+    /**
+     * @Route("/blog/article/12", name="blog_show")
+     */
+    public function show(): Response
+    {
+        return $this->render('blog/show.html.twig');
+    }
+}
