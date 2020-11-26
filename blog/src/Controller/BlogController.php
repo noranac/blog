ab131c183d74a47431d11cc5b42672dd97f7ce99
Nora Naceri <nora.naceri90@gmail.com>
2020-11-26 16:57:41 +0100

    affichage de la liste des articles

--- blog/src/Controller/BlogController.php
+++ blog/src/Controller/BlogController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ArticleRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,12 @@ class BlogController extends AbstractController
     /**
      * @Route("/blog", name="blog")
      */
-    public function index(): Response
+    public function index(ArticleRepository $articleRepository)
     {
+        $articles = $articleRepository->findAll();
+
         return $this->render('blog/index.html.twig', [
-            'controller_name' => 'BlogController',
+            'articles' => $articles,
         ]);
     }
 
